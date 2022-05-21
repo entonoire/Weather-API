c5d9560ad636ebea19432b76b29d6c035e0bae81
entonoire <43643450+entonoire@users.noreply.github.com>
2022-05-21 14:13:37 +0200

    Update Main.java

--- src/fr/vallfeur/test/Main.java
+++ src/fr/vallfeur/test/Main.java
@@ -10,9 +10,6 @@ import fr.vallfeur.wapi.WeatherInfos;
 public class Main {
 
 	public static void main(String[] str) throws IOException{
-		Scanner scanner = new Scanner(new File("F:\\DEVELOPEMENT\\JAVA\\Eclipse\\DiscordRCP\\api.txt"));
-		String[] data = scanner.nextLine().split(",");
-		scanner.close();
 		
 		Weather api = new Weather("ba61e79a0119a86f77cc47164345", "Paris", WeatherInfos.Metric, WeatherInfos.French);
 		System.out.println(api.getTemp());
